e3f633afbd2800d7e4b5972d23f99d103f77f056
Steven <sb.nb.lego@Gmail.com>
2023-03-13 13:17:27 +1300

    Added Firework Class

---
+++ Fireworks Firing Systems/Firework.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+using System;
+
+namespace Fireworks_Firing_Systems
+{
+    public class Firework
+    {
+        public int ID { get; set; }
+        public string FireworkName { get; set; }
+        public FireworkType Type { get; set; }
+        public Color Colour { get; set; }
+        public int Delay { get; set; }
+        public int Length { get; set; }
+
+        public Firework() { }
+        public Firework(string Json)
+        {
+            Firework input = Newtonsoft.Json.JsonConvert.DeserializeObject<Firework>(Json);
+            ID = input.ID;
+            FireworkName = input.FireworkName;
+            Type = input.Type;
+            Colour = input.Colour;
+            Delay = input.Delay;
+            Length = input.Length;
+        }
+        public Firework(int ID_, string Name_, FireworkType Type_, Color Colour_, int Delay_, int Length_)
+        {
+            ID = ID_;
+            FireworkName = Name_;
+            Type = Type_;
+            Colour = Colour_;
+            Delay = Delay_;
+            Length = Length_;
+        }
+
+        public override string ToString() => $"[{ID.ToString()}]{FireworkName}:{Type}({Colour})";
+        public string ToJson() => Newtonsoft.Json.JsonConvert.SerializeObject(this);
+    }
+
+    /// <summary>
+    /// https://www.fireworkshawkesbay.co.nz/types-of-fireworks#
+    /// </summary>
+    public enum FireworkType
+    {
+        Sparkler,
+        Fountain,
+        Smoke_Ball,
+        Missile,
+        Mine,
+        Cake,
+        Roman_Candle
+    }
+}
