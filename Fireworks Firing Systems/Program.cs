dab475bc3af3a1c9751e9159c071b3442c8fe9f4
steven brown <sb.nb.lego@Gmail.com>
2023-05-10 11:04:32 +1200

    Remove Form

--- Fireworks Firing Systems/Program.cs
+++ Fireworks Firing Systems/Program.cs
@@ -11,7 +11,7 @@ namespace Fireworks_Firing_Systems
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new BaseForm());
         }
     }
 }
