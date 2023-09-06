cc22179fd22b892d07cce518bb377c34b27e182c
steven brown <sb.nb.lego@Gmail.com>
2023-09-06 17:54:37 +1200

    Added Libary

--- Arduino Code/Fierwork_Main_Box/Fierwork_Main_Box.ino
+++ Arduino Code/Fierwork_Main_Box/Fierwork_Main_Box.ino
@@ -1,6 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Centipede.h>
 
+Centipede CS;
+
+void setup() {
+  Wire.begin(); // start I2C
+ 
+  CS.initialize(); // set all registers to default
+ 
+  CS.portMode(0, 0b0000000000000000); // set all pins on chip 0 to output (0 to 15)
+  CS.portMode(1, 0b0000000000000000); // set all pins on chip 1 to output (0 to 15)
+  CS.portMode(2, 0b0000000000000000); // set all pins on chip 2 to output (0 to 15)
+  CS.portMode(3, 0b0000000000000000); // set all pins on chip 3 to output (0 to 15)
 }
 
 void loop() {
